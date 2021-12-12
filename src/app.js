db56be3606aff2963158b83b48c3ee8e0b009157
itscode29 <helena.codesal@gmail.com>
2021-12-12 19:32:33 +0100

    Added units conversion button for current data

--- src/app.js
+++ src/app.js
@@ -59,34 +59,34 @@ function formatDate(timestamp) {
 // Replace data
 function displayTemperature(response) {
   let temperatureElment = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+  celsiusMinTemp = response.data.main.temp_min;
+  celsiusMaxTemp = response.data.main.temp_max;
+  metricWindSpeed = response.data.wind.speed;
+  let temperature = Math.round(celsiusTemperature);
   let degreeElement = document.querySelector("#degree");
   temperatureElment.innerHTML = temperature;
   // Center temperatures below 10
   if (temperature < 10 && temperature > -10) {
-    temperatureElment.classList.remove("col-8");
-    temperatureElment.classList.add("col-7");
-    degreeElement.classList.remove("col-4");
-    degreeElement.classList.add("col-5");
+    temperatureElment.classList.replace("col-8", "col-7");
+    degreeElement.classList.replace("col-4", "col-5");
   } else {
-    temperatureElment.classList.remove("col-7");
-    temperatureElment.classList.add("col-8");
-    degreeElement.classList.remove("col-5");
-    degreeElement.classList.add("col-4");
+    temperatureElment.classList.replace("col-7", "col-8");
+    degreeElement.classList.replace("col-5", "col-4");
   }
   //
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let minElement = document.querySelector("#current-min-temp");
-  minElement.innerHTML = Math.round(response.data.main.temp_min);
+  minElement.innerHTML = Math.round(celsiusMinTemp);
   let maxElement = document.querySelector("#current-max-temp");
-  maxElement.innerHTML = Math.round(response.data.main.temp_max);
+  maxElement.innerHTML = Math.round(celsiusMaxTemp);
   let desciptionElement = document.querySelector("#description");
   desciptionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
   let windSpeedElement = document.querySelector("#wind-speed");
-  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  windSpeedElement.innerHTML = Math.round(metricWindSpeed);
   // Change time according to location
   let timeElement = document.querySelector("#time");
   let timestamp = response.data.dt;
@@ -109,7 +109,45 @@ function searchCity(event) {
   showWeather(cityInputElement.value);
 }
 
-showWeather("Barcelona");
+function changeUnits() {
+  debugger;
+  let mainTempElement = document.querySelector("#temperature");
+  let minTempElement = document.querySelector("#current-min-temp");
+  let maxTempElement = document.querySelector("#current-max-temp");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let windUnitsElement = document.querySelector("#wind-units");
+  if (farenheitButton.classList.contains("btn-farenheit")) {
+    let farenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    mainTempElement.innerHTML = Math.round(farenheitTemperature);
+    let farenheitMinTemp = (celsiusMinTemp * 9) / 5 + 32;
+    minTempElement.innerHTML = Math.round(farenheitMinTemp);
+    let farenheitMaxTemp = (celsiusMaxTemp * 9) / 5 + 32;
+    maxTempElement.innerHTML = Math.round(farenheitMaxTemp);
+    let imperialWindSpeed = metricWindSpeed * 2.237;
+    windSpeedElement.innerHTML = Math.round(imperialWindSpeed);
+    windUnitsElement.innerHTML = "mph";
+    farenheitButton.classList.replace("btn-farenheit", "btn-celsius");
+    farenheitButton.innerHTML = "ºC";
+  } else {
+    mainTempElement.innerHTML = Math.round(celsiusTemperature);
+    minTempElement.innerHTML = Math.round(celsiusMinTemp);
+    maxTempElement.innerHTML = Math.round(celsiusMaxTemp);
+    windSpeedElement.innerHTML = Math.round(metricWindSpeed);
+    windUnitsElement.innerHTML = "m/s";
+    farenheitButton.classList.replace("btn-celsius", "btn-farenheit");
+    farenheitButton.innerHTML = "ºF";
+  }
+}
+
+let celsiusTemperature = null;
+let celsiusMinTemp = null;
+let celsiusMaxTemp = null;
+let metricWindSpeed = null;
+
+let farenheitButton = document.querySelector("#btn-farenheit");
+farenheitButton.addEventListener("click", changeUnits);
 
 let searchEngine = document.querySelector("#search-engine");
 searchEngine.addEventListener("submit", searchCity);
+
+showWeather("Barcelona");
