d49fa39336b36a9b116213bb512666e863f08327
itscode29 <helena.codesal@gmail.com>
2021-12-18 18:40:05 +0100

    Organized functions

--- src/app.js
+++ src/app.js
@@ -1,74 +1,6 @@
-function formatTime(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  return `${hours}:${minutes}`;
-}
-
-function formatDate(timestamp) {
-  function getOrdinal(timestamp) {
-    let date = new Date(timestamp);
-    let dayDate = date.getDate();
-    if (dayDate === 1) {
-      return "st";
-    } else if (dayDate === 2 || dayDate === 22) {
-      return "nd";
-    } else if (dayDate === 3) {
-      return "rd";
-    } else {
-      return "th";
-    }
-  }
-  let date = new Date(timestamp);
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let month = months[date.getMonth()];
-  let dayDate = date.getDate();
-  let ordinal = getOrdinal();
-  return `${day}, ${month} ${dayDate}${ordinal}`;
-}
-
-function getVariables() {}
-
-// Replace data
-function displayTemperature(response) {
+function centerTemperatures(temperature) {
   let temperatureElment = document.querySelector("#temperature");
-  celsiusTemperature = response.data.main.temp;
-  celsiusMinTemp = response.data.main.temp_min;
-  celsiusMaxTemp = response.data.main.temp_max;
-  metricWindSpeed = response.data.wind.speed;
-  let temperature = Math.round(celsiusTemperature);
   let degreeElement = document.querySelector("#degree");
-  temperatureElment.innerHTML = temperature;
-  // Center temperatures below 10
   if (temperature < 10 && temperature > -10) {
     temperatureElment.classList.replace("col-8", "col-7");
     degreeElement.classList.replace("col-4", "col-5");
@@ -76,29 +8,9 @@ function displayTemperature(response) {
     temperatureElment.classList.replace("col-7", "col-8");
     degreeElement.classList.replace("col-5", "col-4");
   }
-  //
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
-  let minElement = document.querySelector("#current-min-temp");
-  minElement.innerHTML = Math.round(celsiusMinTemp);
-  let maxElement = document.querySelector("#current-max-temp");
-  maxElement.innerHTML = Math.round(celsiusMaxTemp);
-  let desciptionElement = document.querySelector("#description");
-  desciptionElement.innerHTML = response.data.weather[0].description;
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.main.humidity;
-  let windSpeedElement = document.querySelector("#wind-speed");
-  windSpeedElement.innerHTML = Math.round(metricWindSpeed);
-  // Change time according to location
-  let timeElement = document.querySelector("#time");
-  let timestamp = response.data.dt;
-  let timezone = response.data.timezone;
-  timeElement.innerHTML = formatTime((timestamp + (timezone - 3600)) * 1000);
-  // Change date
-  let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate((timestamp + (timezone - 3600)) * 1000);
-  // Change background
-  let weather = response.data.weather[0].main;
+}
+
+function changeBackground(weather) {
   let backgroundElement = document.querySelector("#background");
   if (weather === "Clouds") {
     backgroundElement.classList.remove(
@@ -197,6 +109,98 @@ function displayTemperature(response) {
   }
 }
 
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function getOrdinal(timestamp) {
+  let date = new Date(timestamp);
+  let dayDate = date.getDate();
+  if (dayDate === 1) {
+    return "st";
+  } else if (dayDate === 2 || dayDate === 22) {
+    return "nd";
+  } else if (dayDate === 3) {
+    return "rd";
+  } else {
+    return "th";
+  }
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let dayDate = date.getDate();
+  let ordinal = getOrdinal(timestamp);
+  return `${day}, ${month} ${dayDate}${ordinal}`;
+}
+
+function changeTimeDate(timestamp, timezone) {
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatTime((timestamp + (timezone - 3600)) * 1000);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate((timestamp + (timezone - 3600)) * 1000);
+}
+
+// Replace data
+function displayTemperature(response) {
+  celsiusTemperature = response.data.main.temp;
+  celsiusMinTemp = response.data.main.temp_min;
+  celsiusMaxTemp = response.data.main.temp_max;
+  metricWindSpeed = response.data.wind.speed;
+  let temperature = Math.round(celsiusTemperature);
+  let temperatureElment = document.querySelector("#temperature");
+  temperatureElment.innerHTML = temperature;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let minElement = document.querySelector("#current-min-temp");
+  minElement.innerHTML = Math.round(celsiusMinTemp);
+  let maxElement = document.querySelector("#current-max-temp");
+  maxElement.innerHTML = Math.round(celsiusMaxTemp);
+  let desciptionElement = document.querySelector("#description");
+  desciptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = Math.round(metricWindSpeed);
+  centerTemperatures(temperature);
+  changeTimeDate(response.data.dt, response.data.timezone);
+  changeBackground(response.data.weather[0].main);
+}
+
 // API Request
 function showWeather(city) {
   let apiKey = "055ee8048e7236318bbd1ee44ad667e0";
@@ -241,7 +245,7 @@ function changeUnits() {
   }
 }
 
-// Show current location data
+// Get current location data
 function searchCurrentLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
